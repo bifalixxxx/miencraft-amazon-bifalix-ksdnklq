be0dc588ed335db04ed400ef11a0812d3b3712c7
bifalixxxx <monteur.pro.bifalix@gmail.com>
2025-08-24 19:36:12 +0200

    Create api.js

---
+++ api.js
@@ -0,0 +1,13 @@
+// CONFIGURATION API
+
+// Feuille de stock des items à vendre
+const stockURL =
+  "https://docs.google.com/spreadsheets/d/TON_ID_DU_STOCK/export?format=csv";
+
+// Feuille de suivi des commandes
+const commandeURL =
+  "https://docs.google.com/forms/d/TON_ID_DU_FORMULAIRE_FORM/FORMResponse"; // webhook
+
+// Feuille de statut des commandes
+const statutURL =
+  "https://docs.google.com/spreadsheets/d/TON_ID_DU_STATUT/export?format=csv";
